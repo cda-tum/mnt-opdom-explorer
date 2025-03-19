f4e173ed638721050d1e44a58edb8ba643b4c0b7
Marcel Walter <marcel.walter@tum.de>
2025-03-19 09:55:48 +0100

    :green_heart: Fix build requirements in `noxfile.py`

--- noxfile.py
+++ noxfile.py
@@ -18,11 +18,7 @@ nox.options.default_venv_backend = "uv|virtualenv"
 
 PYTHON_ALL_VERSIONS = ["3.9", "3.10", "3.11", "3.12"]
 
-BUILD_REQUIREMENTS = [
-    "setuptools>=66.1",
-    "setuptools_scm>=8.1",
-    "wheel>=0.40",
-]
+BUILD_REQUIREMENTS = ["hatchling>=1.27.0", "hatch-vcs>=0.4.0"]
 
 if os.environ.get("CI", None):
     nox.options.error_on_missing_interpreters = True
