7f87cd9ffcf41215fdf54d9b973bed288a42ccff
Marcel Walter <marcel.walter@tum.de>
2025-05-17 12:17:02 +0200

    :sparkles: Add PixmapConversionService for converting Matplotlib Figures to QPixmap

--- src/mnt/ode/services/__init__.py
+++ src/mnt/ode/services/__init__.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 from .layout_visualization_service import LayoutVisualizationError, LayoutVisualizationService
 from .operational_domain_plotting_service import OperationalDomainPlottingService, PlottingError
 from .operational_domain_service import OperationalDomainError, OperationalDomainService
+from .pixmap_conversion_service import PixmapConversionService
 from .simulation_service import SimulationError, SimulationService
 from .sqd_file_service import LayoutLoadError, SQDFileService
 
@@ -15,6 +16,7 @@ __all__ = [
     "OperationalDomainError",
     "OperationalDomainPlottingService",
     "OperationalDomainService",
+    "PixmapConversionService",
     "PlottingError",
     "SQDFileService",
     "SimulationError",
---
+++ src/mnt/ode/services/pixmap_conversion_service.py
@@ -0,0 +1,79 @@
+"""Service to convert Matplotlib Figures to QPixmap for PyQt6."""
+
+from __future__ import annotations
+
+import io
+import logging
+from typing import TYPE_CHECKING
+
+from PyQt6.QtCore import QByteArray, QSize, Qt
+from PyQt6.QtGui import QImage, QPainter, QPixmap
+from PyQt6.QtSvg import QSvgRenderer
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+    from matplotlib.figure import Figure
+
+logger = logging.getLogger(__name__)
+
+
+class PixmapConversionService:
+    """Service to convert Matplotlib Figures to QPixmap for PyQt6."""
+
+    @staticmethod
+    def convert(figures: Sequence[Figure | None]) -> list[QPixmap]:
+        """Convert a list of Matplotlib Figures to QPixmaps.
+
+        Args:
+            figures: List of Matplotlib Figures to convert.
+
+        Returns:
+            List of QPixmaps corresponding to the input figures.
+        """
+        pixmaps = []
+        for fig in figures:
+            if fig is not None:
+                try:
+                    svg_bytes = PixmapConversionService._figure_to_svg_bytes(fig)
+                    pixmap = PixmapConversionService._svg_bytes_to_pixmap(svg_bytes)
+                    pixmaps.append(pixmap)
+                except Exception:
+                    logger.exception("Failed to convert figure to pixmap.")
+                    pixmaps.append(QPixmap())
+            else:
+                pixmaps.append(QPixmap())
+        return pixmaps
+
+    @staticmethod
+    def _figure_to_svg_bytes(fig: Figure) -> bytes:
+        """Convert a Matplotlib Figure to SVG bytes.
+
+        Args:
+            fig: The Matplotlib Figure to convert.
+
+        Returns:
+            SVG bytes representing the figure.
+        """
+        buf = io.BytesIO()
+        fig.savefig(buf, format="svg", bbox_inches="tight")
+        return buf.getvalue()
+
+    @staticmethod
+    def _svg_bytes_to_pixmap(svg_bytes: bytes) -> QPixmap:
+        """Convert SVG bytes to a QPixmap using QSvgRenderer.
+
+        Args:
+            svg_bytes: The SVG bytes to convert.
+
+        Returns:
+            A QPixmap representing the SVG.
+        """
+        renderer = QSvgRenderer(QByteArray(svg_bytes))
+        base_w, base_h = 800, 800
+        image = QImage(QSize(base_w, base_h), QImage.Format.Format_ARGB32)
+        image.fill(Qt.GlobalColor.transparent)
+        painter = QPainter(image)
+        renderer.render(painter)
+        painter.end()
+        return QPixmap.fromImage(image)
